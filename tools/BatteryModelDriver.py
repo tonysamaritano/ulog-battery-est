f494b3d841bd3e9e0f3c35dac41a00912bd597c2
Ryan-Oliver-2k <60796472+Ryan-Oliver-2k@users.noreply.github.com>
2022-05-27 16:10:58 -0400

    Updated Code from Review Feedback

--- tools/BatteryModelDriver.py
+++ tools/BatteryModelDriver.py
@@ -1,3 +1,5 @@
+# Driver script for battery model with sample inputs
+
 import sys
 import pyulog as ulog
 import matplotlib.pyplot as plt
@@ -11,12 +13,12 @@ assert len(sys.argv) > 1, f"No input file specified"  # Verify file provided
 print(f"input: {sys.argv[1]}")  # Print file name
 
 # Test 1
-print("making battery model object")
-battery_model_1 = BatteryModel(11.1, 5100, 3, sys.argv[1])
+print("Making battery model object")
+battery_model_1 = BatteryModel(sys.argv[1])
 
-print("printing data")
+print("Printing data")
 battery_model_1.print_data()
-print("graphing power data")
+print("Graphing power data")
 battery_model_1.graph_power_data()
-print("graphing capacity data")
+print("Graphing capacity data")
 battery_model_1.graph_capacity_data()

6e14b0c53e94f846f5b8448c7f2ae4bce1521801
gdonados <donado24@students.rowan.edu>
2022-06-03 13:38:27 -0400

    Revised based on comments

--- tools/BatteryModelDriver.py
+++ tools/BatteryModelDriver.py
@@ -7,12 +7,11 @@ import numpy as np
 
 from BatteryModel import BatteryModel
 
-assert len(sys.argv) > 1, f"No input file specified"  # Verify file provided
+assert len(sys.argv) == 2, f"No input voltage specified"
+try:
+    voltage = float(sys.argv[1])
+except ValueError:
+    assert False, f"Input {sys.argv[1]} not a float"
 
-print(f"input: {sys.argv[1]}")  # Print file name
-
-# Test 1
-print("Making battery model object")
-battery_model_1 = BatteryModel(sys.argv[1])
-
-print(battery_model_1.simulate(12.3)/60)
+battery_model_1 = BatteryModel()
+print(battery_model_1.simulate(voltage))

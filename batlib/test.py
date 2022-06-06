4043c89bb534fba4cb4a20f80a58205a27450cd7
Ryan-Oliver-2k <60796472+Ryan-Oliver-2k@users.noreply.github.com>
2022-06-06 10:36:23 -0400

    unit test file formatting

---
+++ batlib/test.py
@@ -0,0 +1,5 @@
+def func():
+    return "hello world"
+
+def another_func():
+    return "something"
---
+++ testing/test_test.py
@@ -0,0 +1,5 @@
+import pytest
+from batlib.test import func
+
+def test_test():
+    assert func() == "hello world"

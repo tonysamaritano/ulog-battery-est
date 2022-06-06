440468dc520c24c1dbec5426953b3b61797f7b8d
gdonados <donado24@students.rowan.edu>
2022-06-06 16:11:52 -0400

    First model iteration

--- batlib/model.py
+++ batlib/model.py
@@ -31,19 +31,37 @@ class Model:
         self.current = 0
         self.temperature = 0
         self.capacity = 0
+        self.capacity_array = np.array(np.ones(100))
+        self.rolling_average = 0
 
-        # Arm boolean
+        # Arm boolean, capacity init boolean
         self.armed = False
+        self.capacity_initialized = False
 
         self.time_estimation = 0.0
 
-    def update(self, time):
+    def update(self, dt) -> float:
         """
         Continuous update loop of battery information
 
         :param time: time elapsed 
+
+        :return: estimated time remaining in seconds
         """
-        pass
+        # Initialize capacity
+        if(not self.capacity_initialized):
+            self.__init_capacity__()
+
+        # Decrement current draw, assuming current is read as mA
+        # *************************************************************************************************
+        # *** will dt be continuously summed/overall time, or be passed as a duration since last update ***
+        # *************************************************************************************************
+        self.capacity = self.capacity - (self.current*(dt/3600))
+
+        # Perform estimations on either real or experimental data, depedning on arm
+        self.time_estimation = self.__equation__(
+            self.e, self.f, self.g, self.h, self.capacity) if self.armed else self.__equation__(
+                self.e, self.f, self.g, self.h, self.__equation__(self.a, self.b, self.c, self.d, self.voltage))
 
     def getTimeEstimate(self) -> float:
         """
@@ -65,22 +83,32 @@ class Model:
         self.current = current
         self.temperature = temperature
 
-    def setArmed(self) -> None:
+    def setArmed(self, set: bool) -> None:
         """
         Indicate arming of drone
+
+        :param set: true/false for enabling/disabling arm
         """
-        self.armed = True
+        self.armed = set
 
     def __init_capacity__(self) -> bool:
         """
         Iterate and average rate of change of battery capacity to settle on an initial value
         """
+        smallest_difference = 50
+        difference_check = 0
+        new_average = 0
 
-        pass
+        new_average = (self.rolling_average - self.rolling_average / 5) + (
+            self.rolling_average + self.__equation__(self.a, self.b, self.c, self.d, self.voltage) / 5)
 
-    def __voltage_to_capacity__(self, a: float, b: float, c: float, d: float, voltage: float) -> float:
+        difference_check = self.rolling_average - new_average
+
+        return True if difference_check < smallest_difference else False
+
+    def __equation__(self, a: float, b: float, c: float, d: float, input: float) -> float:
         """
-        3rd order polynomial estimation of capacity form voltage value
+        3rd order polynomial estimation of capacity fromm input value (voltage or capacity)
 
         :param voltage: input voltage
         :param a: 3rd degree coefficient
@@ -89,17 +117,4 @@ class Model:
         :param d: constant coefficient
         :return: resulting capacity estimate
         """
-        return (a*voltage**3) + (b*voltage**2) + (c*voltage) + d
-
-    def __capacity_to_time__(self, a: float, b: float, c: float, d: float, capacity: float) -> float:
-        """
-        3rd order polynomial estimation of time form capacity value
-
-        :param capacity: input capacity
-        :param a: 3rd degree coefficient
-        :param b: 2nd degree coefficient
-        :param c: 1st degree coefficient
-        :param d: constant coefficient
-        :return: resulting time estimate
-        """
-        return (a*capacity**3) + (b*capacity**2) + (c*capacity) + d
+        return (a*input**3) + (b*input**2) + (c*input) + d
